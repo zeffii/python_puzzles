d9b7aeb4d9c760683461cd54326d355c7e39b700
Dealga McArdle <zeffii@users.noreply.github.com>
2017-05-25 11:06:20 +0200

    Create 001_mask_polygon_subset.py

---
+++ 001_mask_polygon_subset.py
@@ -0,0 +1,12 @@
+
+
+all_polygons = [[0,1,2],[1,2,3],[2,3,4],[3,4,5],[5,6,7]]
+subset_polygons = [[0,1,2],[3,4,5]]
+
+
+def make_subset_mask(polygons, subset):
+    ...
+
+
+right_answer = [1,0,0,1,0]
+assert(right_answer == make_subset_mask(all_polygons, subset_polygons))
